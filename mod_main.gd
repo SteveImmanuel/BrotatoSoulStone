0ab05d6b0326039d67b8cab930c93ba8ff8b3e78
SteveImmanuel <steveandreasimanuel@gmail.com>
2023-04-25 17:29:33 +0900

    Init structure

---
+++ mod_main.gd
@@ -0,0 +1,18 @@
+extends Node
+
+const MYMODNAME_MOD_DIR = "SteveImm-SoulStone/"
+const MYMODNAME_LOG = "SteveImm-SoulStone"
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
