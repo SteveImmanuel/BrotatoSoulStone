967d34c3c591176cb8dfa2ed5d48070e1b111c5a
SteveImmanuel <steveandreasimanuel@gmail.com>
2023-04-28 11:42:02 +0900

    Load extensions

--- mod_main.gd
+++ mod_main.gd
@@ -4,22 +4,24 @@ const MOD_DIR = "SteveImm-SoulStone/"
 const MOD_LOG = "SteveImm-SoulStone"
 
 var dir = ""
-var ext_dir = ""
-var trans_dir = ""
-
 
 func _init(modLoader = ModLoader):
 	ModLoaderUtils.log_info("Init", MOD_LOG)
 	dir = modLoader.UNPACKED_DIR + MOD_DIR
-	ext_dir = dir + "extensions/"
-	# trans_dir = dir + "translations/"
+	var ext_dir = dir + "extensions/"
+	var trans_dir = dir + "translations/"
 
 	# Add extensions
 	modLoader.install_script_extension(ext_dir + "main.gd")
+	modLoader.install_script_extension(ext_dir + "singletons/run_data.gd")
 
 	# Add translations
-	# modLoader.add_translation_from_resource(trans_dir + "modname_text.en.translation")
+	modLoader.add_translation_from_resource(trans_dir + "soulstone_text.en.translation")
 
 func _ready():
+	var ContentLoader = get_node("/root/ModLoader/Darkly77-ContentLoader/ContentLoader")
+	var content_dir = dir + "content_data/"
+	
+	ContentLoader.load_data(content_dir + "soulstone_items.tres", MOD_LOG)
 	ModLoaderUtils.log_info("Done", MOD_LOG)
 	
